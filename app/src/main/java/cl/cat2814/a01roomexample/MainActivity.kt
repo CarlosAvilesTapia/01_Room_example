8980d2cfd92da81c0baa071fcb4095f83bb579cd
CAT <cavilest@gmail.com>
2023-07-26 20:22:28 -0400

    Ejercicio individual 1 - Se implementa navigation. - Se crean 2 fragments.

--- app/src/main/java/cl/cat2814/a01roomexample/MainActivity.kt
+++ app/src/main/java/cl/cat2814/a01roomexample/MainActivity.kt
@@ -4,8 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    /*
+    [] Navigation
+    [] 1 fragment agregar elementos
+    [] Interface
+    [] Room
+        [] DAO
+        [] BD
+        [] Entity
+
+    [] 1 fragment listado
+    [] RV
+
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
